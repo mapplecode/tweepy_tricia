111aad3d70b5623ddf4ce118c6a054a49c5da801
Mapplecode <mapplecode2020@gmail.com>
2022-06-14 16:30:30 +0530

    pip setup

--- user_tweets.py
+++ user_tweets.py
@@ -3,6 +3,7 @@ import tweepy
 from creds import CREDS as credentials
 # function to display data of each tweet
 import csv
+import  time
 consumer_key = credentials['API_Key']
 consumer_secret = credentials['API_Key_Secret']
 access_token = credentials['Access_token']
@@ -23,20 +24,40 @@ tweets = api.user_timeline(screen_name=userID,
                            tweet_mode='extended'
                            )
 # print(tweets)
-data=[]
-for info in tweets[:3]:
-     print("ID: {}".format(info.id))
 
-     retweets_list = api.get_retweets(info.id)
-     # folowers = api.get_followers(userID)
-     # for folowe in folowers:
-     #     print(folowe.screen_name)
-     users_retweeted = ''
-     # printing the screen names of the retweeters
-     for retweet in retweets_list:
-         users_retweeted += retweet.user.screen_name+' , '
-     data.append(["ID: {}".format(info.id), info.created_at, info.full_text,users_retweeted])
+user = api.get_user(screen_name=userID)
+mentions = api.mentions_timeline()
+print('mentions total - ' ,len(mentions))
+followers = api.get_followers(screen_name=userID,)
+print('followers total - ' ,len(followers))
+data=[]
+import time
+for info in tweets[-10:]:
+    time.sleep(1)
+    try:
+         print("ID: {}".format(info.id))
+
+         # for k,v in info.__dict__.items():
+         #     print(k,v)
+         retweets_list = api.get_retweets(info.id)
+         created_time= info.created_at
+         print(str(created_time))
+         # folowers = api.get_followers(userID)
+         # for folowe in folowers:
+         #     print(folowe.screen_name)
+         users_retweeted = ''
+         # printing the screen names of the retweeters
+         for retweet in retweets_list:
+             users_retweeted += retweet.user.screen_name+' , '
+         data.append(["ID: {}".format(info.id), info.created_at, info.full_text,users_retweeted,info.retweet_count,
+                      info.favorite_count,info.retweet_count])
+    except Exception as e:
+        print(e)
+
 with open(userID+'scrap.csv', 'w') as f:
     write = csv.writer(f)
+    write.writerow(['USERNAME - '+str(userID),'Mentions - '+str(len(mentions)) ,'Followers - '+str(len(followers))])
+    write.writerow(['ID','Created time','Full tweet text','User retweeted' , 'Retweet count',
+                    'Liked count','Retweet count'])
     for info in data:
         write.writerow(info)
