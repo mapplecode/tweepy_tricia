bb2f062914313c0780aadefa9c61cdda30c063ea
Mapplecode <59723349+Mapplecode@users.noreply.github.com>
2022-07-14 11:49:42 +0700

    creds change

--- creds.py
+++ creds.py
@@ -1,9 +1,23 @@
 
 CREDS = dict(
-Access_token= "2479789014-7rp897xNB2Wa0IIcVzj9InKU6VGnBkSH9NqSZZv",
-Access_token_secret= "hyNQsAAgwYe8fg1xocD2bMKTTF0D9P5Ihpi8OM1DZclDy",
-API_Key = "rjC6RtfKq64MnNDUBAQtAAXXb",
-API_Key_Secret = "SgYxdZDwrHDjYMc34LHARYJeua906xqHSTQeGvLFzoyvmxez3m",
-Bearer_Token = "AAAAAAAAAAAAAAAAAAAAAKGadQEAAAAA7Zmz6BmapwohvMglFAFLp4xOZos%3DeURmMmG86xaqwgp8Pzc9rT6XTAYZCnF0URKdhK0jaQq7kbwFAy",
+Access_token= "2479789014-mrEVejOAqJWMlVz4DKLnqwvImOzXupr1Wv1RON6",
+Access_token_secret= "cwOgobbHbWuuoocLpQKrVTsFRFXBiB7rpAd3F1bHpwisC",
+API_Key = "N5HsekWjW4n5rj4HsOroOH71a",
+API_Key_Secret = "ZwObjkw9hrkquXsqmlu49ZBKeUw7YZ7gwykwC16jUCOzaSW3xm",
+Bearer_Token = "AAAAAAAAAAAAAAAAAAAAAKGadQEAAAAAYix%2Fv%2BJ4cKZUXd3r2%2FuY1SgmFV8%3DQfqudNL9y4r0TnXoIKhM0WhoKa84oBchgbi3vvqMy916pc23FV",
 NAME= "UK MP Mapping",
 APP_ID=  24484513)
+
+
+
+gggg="""
+API key N5HsekWjW4n5rj4HsOroOH71a
+
+API secret key: ZwObjkw9hrkquXsqmlu49ZBKeUw7YZ7gwykwC16jUCOzaSW3xm
+
+Bearer token: AAAAAAAAAAAAAAAAAAAAAKGadQEAAAAAYix%2Fv%2BJ4cKZUXd3r2%2FuY1SgmFV8%3DQfqudNL9y4r0TnXoIKhM0WhoKa84oBchgbi3vvqMy916pc23FV
+
+Access token: 2479789014-mrEVejOAqJWMlVz4DKLnqwvImOzXupr1Wv1RON6
+
+Access token secret: cwOgobbHbWuuoocLpQKrVTsFRFXBiB7rpAd3F1bHpwisC
+"""
