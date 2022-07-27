ddf5e70df2f3093d2f74f7bcd956b661784dfbe9
Mapplecode <59723349+Mapplecode@users.noreply.github.com>
2022-07-27 17:35:54 +0530

    creds change

--- creds.py
+++ creds.py
@@ -1,12 +1,13 @@
 
 CREDS = dict(
-Access_token= "2479789014-mrEVejOAqJWMlVz4DKLnqwvImOzXupr1Wv1RON6",
-Access_token_secret= "cwOgobbHbWuuoocLpQKrVTsFRFXBiB7rpAd3F1bHpwisC",
+Access_token= "2479789014-hu8i7DOPsEz1J1DL7rV7ppE6wQOgRmlXvOlzTNf",
+Access_token_secret= "DPvRWelATwly7l9afU7eXcSxht5I23P4OrxKmPKTYyPkF",
 API_Key = "N5HsekWjW4n5rj4HsOroOH71a",
 API_Key_Secret = "ZwObjkw9hrkquXsqmlu49ZBKeUw7YZ7gwykwC16jUCOzaSW3xm",
 Bearer_Token = "AAAAAAAAAAAAAAAAAAAAAKGadQEAAAAAYix%2Fv%2BJ4cKZUXd3r2%2FuY1SgmFV8%3DQfqudNL9y4r0TnXoIKhM0WhoKa84oBchgbi3vvqMy916pc23FV",
 NAME= "UK MP Mapping",
-APP_ID=  24484513)
+APP_ID=  24484513,
+LABEL='2022Dev')
 
 
 
--- twilio_req_all.py
+++ twilio_req_all.py
@@ -1,23 +1,47 @@
 import tweepy
-from creds import CREDS as credentials
+from mycreds import CREDS as credentials
 consumer_key = credentials['API_Key']
 consumer_secret = credentials['API_Key_Secret']
 access_token = credentials['Access_token']
 access_token_secret = credentials['Access_token_secret']
 app_name = credentials['NAME']
+label = credentials['LABEL']
+Bearer_token = credentials['Bearer_Token']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+token = auth.get_authorization_url()
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 search_words = "JulianSmithUK"
 date_since = "01-01-2022"
 date_since_pro = "202008130000"
 numTweets = 100
-
+client = tweepy.Client( consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,)
+print(client.access_token)
 # standart search
 # tweets = tweepy.Cursor(api.search_tweets, q=search_words, since=date_since).items(numTweets)
 
 # premium search
-tweets=api.search_full_archive(environment_name=app_name, query=search_words, since=date_since,label=app_name)
+tweets=api.search_full_archive( query=search_words,label=label)
 
-
-print(tweets)
+try:
+    for i in tweets[-1:]:
+        # print(i.__dict__)
+        print("ID: {}".format(i.id))
+        print(i.created_at)
+except Exception as e:
+    print(e)
+try:
+    for i in tweets[:1]:
+        # print(i.__dict__)
+        print("ID: {}".format(i.id))
+        print(i.created_at)
+except Exception as e:
+    print(e)
+# try:
+#     print(tweets.__dict__.keys())
+#     print(tweets)
+# except Exception as e:
+#     print(e)
