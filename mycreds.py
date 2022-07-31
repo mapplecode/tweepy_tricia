507b7ba369e35a2ce0981ee6ab278ac915d357a2
Mapplecode <59723349+Mapplecode@users.noreply.github.com>
2022-08-01 04:10:20 +0530

    all latest tests

---
+++ mycreds.py
@@ -0,0 +1,24 @@
+
+CREDS = dict(
+Access_token= "1533805545022402561-xgOmT22hkPJ7YiJxHCv8ZtmIpdLCuS",
+Access_token_secret= "8xgMcOfOGi2nBCtp5ETU2VBcyFHsO60emizpox5XDJYal",
+API_Key = "ayW0L2Zc6uxYUutL2jdpojlgl",
+API_Key_Secret = "PkwX55Mmk25ilK2vSlwdyyiGJqNGsBLBgfWjTYDp8C0sbSf3QM",
+Bearer_Token = "AAAAAAAAAAAAAAAAAAAAANvSfAEAAAAA4Dff%2FyQD4G81%2BgJO9A4CsE90c6w%3DfjYUqRmDM7zmNJVwdNkgjQCNDuzCxvw4JSIVKmG5lU8u2k5bMI",
+NAME= "mapplecode_app",
+APP_ID=  24957659,
+LABEL='mapplelable2')
+
+
+
+gggg="""
+API key N5HsekWjW4n5rj4HsOroOH71a
+
+API secret key: ZwObjkw9hrkquXsqmlu49ZBKeUw7YZ7gwykwC16jUCOzaSW3xm
+
+Bearer token: AAAAAAAAAAAAAAAAAAAAAKGadQEAAAAAYix%2Fv%2BJ4cKZUXd3r2%2FuY1SgmFV8%3DQfqudNL9y4r0TnXoIKhM0WhoKa84oBchgbi3vvqMy916pc23FV
+
+Access token: 2479789014-mrEVejOAqJWMlVz4DKLnqwvImOzXupr1Wv1RON6
+
+Access token secret: cwOgobbHbWuuoocLpQKrVTsFRFXBiB7rpAd3F1bHpwisC
+"""
